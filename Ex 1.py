3c524b62eff634c635fc180ce2492a97d01ce478
SoonlyT <102710888+SoonlyT@users.noreply.github.com>
2022-04-03 20:09:03 +0700

    Update Ex 1.py

--- Ex 1.py
+++ Ex 1.py
@@ -1,14 +1,4 @@
 def replace_last(array):
-    new_array=[]
-    for x,i in enumerate(array):
-        n=(x+len(array)-1)%(len(array))
-        new_array.append(array[n])
-    new_array
-    return (new_array)
-
-print(replace_last([1, 2, 3, 4]))
-
-def replace_last(array):
     new_array = array[-1:] + array[:-1]
     return new_array
 
