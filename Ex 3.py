260ec70f7093f2f98fa8c23c1663f83557a5cfd6
SoonlyT <102710888+SoonlyT@users.noreply.github.com>
2022-04-03 20:10:52 +0700

    Update Ex 3.py

--- Ex 3.py
+++ Ex 3.py
@@ -1,21 +1,3 @@
-
-def remove_all_after(array,index):
-    new_array=[]
-    n=0
-    for i in array:
-        if i < index:
-            new_array.append(array[n])
-            n+=1
-        elif i == index:
-            i=index
-            new_array.append(array[n])
-            break
-        else:
-            break
-    return new_array
-print (remove_all_after([1, 1, 2, 2, 2, 3, 3], 2))
-
-
 def remove_all_after(numbers, border):
     for i in range(len(numbers)):
         if numbers[i] == border:
